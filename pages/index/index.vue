699e316ccac6a8f9ddc9615db47a15be34371399
YuanChengLang <709364178@qq.com>
2020-12-19 19:13:00 +0800

    first

---
+++ pages/index/index.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="container">
+		<button @click="selectPhoto">获取照片</button>
+		<image :src="imagefile"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				imageUrl: '',
+				imagefile: '',
+			}
+		},
+		methods: {
+			selectPhoto() {
+				uni.chooseImage({
+					count: 1,
+					success: (res) => {
+						console.log(res)
+						this.imagefile = res.tempFilePaths[0]
+					}
+				})
+			}
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
